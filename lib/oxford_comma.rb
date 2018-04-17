4573c73fa3beb674560426ef79af7624d049c250
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-17 20:19:12 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.size < 3
     array.join(" and ")
   else
-    array.last = " and " array.last
+    array.last = " and " + array.last
     string = array.join(", ")
   end
 end
